34cde25c9e5d462abbc7db9978a6c170667b017e
Cesar Morales <cmorales@thedataage.com>
2021-09-01 23:44:25 -0600

    First commit

---
+++ src/GifExpertApp.js
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import { AddCategory } from "./components/AddCategory";
+import { GifGrid } from "./components/GifGrid";
+
+const GifExpertApp = () => {
+    const [categories, setCategories] = useState(['One Punch']);
+
+    // const handleAdd = () => {
+    //     // setCategories([...categories, 'HunterXHunter']);
+    //     setCategories(cats => [...cats, 'HunterXHunter']);
+    // }
+
+    return (
+        <>
+            <h2>GifExpertApp</h2>
+            <AddCategory setCategories={setCategories} />
+            <hr />
+
+            {/* <button onClick={handleAdd}>Agregar</button> */}
+
+            <ol>
+                {
+                    categories.map(category => (
+                        <GifGrid
+                            category={category}
+                            key={category}
+                        />
+                    ))
+                }
+            </ol>
+        </>
+    );
+};
+
+export default GifExpertApp;
